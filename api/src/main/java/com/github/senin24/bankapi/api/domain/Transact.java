14c38a4c23067370374575d7fbad6db832e3cffd
Pavel Senin <senin24@gmail.com>
2018-11-04 18:44:50 +0300

    change to GenerationType.IDENTITY

--- api/src/main/java/com/github/senin24/bankapi/api/domain/Transact.java
+++ api/src/main/java/com/github/senin24/bankapi/api/domain/Transact.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class Transact {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String transactionName, description;

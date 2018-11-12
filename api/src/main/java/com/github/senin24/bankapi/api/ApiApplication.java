d11b7e204178b9d69a8449588af8e1bae1c4c5de
senin24 <senin24@gmail.com>
2018-11-12 20:10:47 +0300

    liquibase in progress

--- api/src/main/java/com/github/senin24/bankapi/api/ApiApplication.java
+++ api/src/main/java/com/github/senin24/bankapi/api/ApiApplication.java
@@ -2,11 +2,20 @@ package com.github.senin24.bankapi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
     }
 }

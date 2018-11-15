ca18ce11c57d27b2fe6db3a46536c8fd8c4f4684
senin24 <senin24@gmail.com>
2018-11-15 16:54:28 +0300

    run H2 as server

--- api/src/main/java/com/github/senin24/bankapi/api/ApiApplication.java
+++ api/src/main/java/com/github/senin24/bankapi/api/ApiApplication.java
@@ -2,7 +2,6 @@ package com.github.senin24.bankapi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.h2.tools.Server;
 
 import java.sql.SQLException;
@@ -10,12 +9,8 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class ApiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Server.createTcpServer().start(); //jdbc:h2:tcp://localhost/mem:testdb
         SpringApplication.run(ApiApplication.class, args);
-    }
-
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
     }
 }
